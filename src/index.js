964700d5407b810f03b56053050f80fb3e4f1fba
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-17 23:03:08 -0600

    Add error response handler

--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ app.use(function (req, res, next) {
     res.status(404).send("Sorry, can't find that!");
 })
 
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Sorry, something went wrong.');
+});
+
 app.listen(3000, function () {
     console.log('Server started, listening on port 3000.');
 });
